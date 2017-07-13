3b01fc8d141fd2920fa9413440fd84a66c812e5d
nuxse <nuxse@nuxsedeMacBook-Air.local>
2017-07-13 10:46:34 +0800

    update ArrayAccess

--- arrayAccess/Books.php
+++ arrayAccess/Books.php
@@ -1,22 +1,23 @@
 <?php
-
+//操作数组一样操作对象
 class Books implements ArrayAccess
 {
-    public $pool = [];
+    private $values = [];
     public function offsetSet($offset, $value)
     {
-        return $this->pool[$offset] = $value;
+        //key 唯一
+        return $this->values[$offset] = $value;
     }
 
     public function offsetExists($offset)
     {
-        return isset($this->pool[$offset]);
+        return isset($this->values[$offset]);
     }
 
     public function offsetGet($offset)
     {
         if($this->offsetExists($offset)) {
-            return $this->pool[$offset];
+            return $this->values[$offset];
         }else{
             throw new Exception("$offset is not exists");
         }
@@ -25,19 +26,14 @@ class Books implements ArrayAccess
     public function offsetUnset($offset)
     {
         if($this->offsetExists($offset)) {
-            unset($this->pool[$offset]);
+            unset($this->values[$offset]);
         }
     }
 
-    public function getPool(): array
-    {
-        return $this->pool;
-    }
 }
 
 $books = new Books();
 $books['a'] = 'a';
 $books['b'] = 'b';
-print_r($books->getPool());
 print_r($books);
-echo $books['c'];
+echo $books['a'];
